4086850567421134bf997d50ad8a8e1105912606
sachin <talathi@gmail.com>
2023-01-24 22:11:32 -0800

    remove absolute path info

--- run_experiments.py
+++ run_experiments.py
@@ -24,8 +24,8 @@ while count<10:
 		if not gpu_under_use(3-k):
 			print count,k
 			str_cmd='THEANO_FLAGS=\'device=gpu%d,lib.cnmem=0.8\' python eeg_classification --train --learning-rate 0.001 --cool --epochs 300 --window-size 80\
-			 --save-dir /home/talathi1/Work/Python/Models/keras_models/rnneeg_models --memo rnn_eeg_model_case0_w80_300epochs_%d\
-			>> /home/talathi1/Work/Python/Models/keras_models/conveeg_models/logfiles/rnn_eeg_model_case0_w80_300epochs_%d.log 2>&1&'%(k,count,count)
+			 --save-dir ~/Python/Models/keras_models/rnneeg_models --memo rnn_eeg_model_case0_w80_300epochs_%d\
+			>> ~/Python/Models/keras_models/conveeg_models/logfiles/rnn_eeg_model_case0_w80_300epochs_%d.log 2>&1&'%(k,count,count)
 			print str_cmd
 			os.system(str_cmd)
 			time.sleep(10)
